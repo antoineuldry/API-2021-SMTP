b00328ef6b52f2d1f78ef8282ec78b86238eca16
antoineuldry <antoine.uldry@heig-vd.ch>
2021-12-05 19:07:23 +0100

    Prank files

--- src/main/java/model/prank/Prank.java
+++ src/main/java/model/prank/Prank.java
@@ -1,4 +1,53 @@
 package model.prank;
 
 public class Prank {
+    private Person victimSender;
+    private final List<Person> victimRecipients = new ArrayList<Person>();
+    private final List<Person> witnessRecipients = new ArrayList<Person>();
+    private String message;
+
+    public Person getVictimSender() {
+        return victimSender;
+    }
+
+    public void setVictimSender(Person victimSender) {
+        this.victimSender = victimSender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void addVictimRecipients(List<Person> victims) {
+        victimRecipients.addAll(victims);
+    }
+
+    public void addVictimRecipients(List<Person> witnesses) {
+        victimRecipients.addAll(witnesses);
+    }
+
+    public List<Person> getVictimRecipients() {
+        return victimRecipients;
+    }
+
+    public List<Person> getWitnessRecipients() {
+        return witnessRecipients;
+    }
+
+    public Message generateMailMessage() {
+        Message msg = new Message();
+
+        msg.setBody(this.message + "\r\n" + victimSender.getFirstName());
+        String[] to = victimRecipients.stream()
+                .map(p -> p.getAddress())
+                .collect(Collectors.toList())
+                .toArray(new String[]{});
+        msg.setCc(cc);
+        msg.setFrom(victimSender.getAddress());
+        return msg;
+    }
 }
--- src/main/java/model/prank/PrankGenerator.java
+++ src/main/java/model/prank/PrankGenerator.java
@@ -1,4 +1,68 @@
 package model.prank;
 
+import config.IConfigManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrankGenerator {
+    private IConfigManager configManager;
+
+    public PrankGenerator(IConfigManager configManager) {
+        this.configManager = configManager;
+    }
+
+    public List<Prank> generatePranks() {
+        List<Prank> pranks = new ArrayList<Prank>();
+
+        List<String> messages = configManager.getMessages();
+        int messageIndex = 0;
+
+        int numberOfGroups = configManager.getNumberOfGroups();
+        int numberOfVictims = configManager.getVictims().size();
+        // There should be at least 5 victims per group
+        if(numberOfVictims / numberOfGroups < 5) {
+            numberOfGroups = numberOfVictims / 5;
+            LOG.warning("Not enough victims to generate the desired number of groups (max groups " + numberOfGroups + ")");
+        }
+
+        List<Group> groups = generateGroups(configManager.getVictims(), numberOfGroups);
+        for (Group group : groups) {
+            Prank prank = new Prank();
+
+            List<Person> victims = group.getMembers();
+            Collections.shuffle(victims);
+            Person sender = victims.remove(0);
+            prank.setVictimSender(sender);
+            prank.addVictimRecipients(victims);
+
+            prank.addWitnessRecipients(configManager.getWitnessesToCC());
+
+            String message = messages.get(messageIndex);
+            messageIndex = (messageIndex + 1) % messages.size();
+            prank.setMessage(message);
+
+            pranks.add(prank);
+        }
+        return pranks;
+    }
+
+    public List<Group> generateGroups(List<Person> victims, int numberOfGroups) {
+        List<Person> availableVictims = new ArrayList<Person>(victims);
+        Collecitons.shuffle(availableVictims);
+        List<Group> groups = new ArrayList<Group>();
+        for (int i = 0; i < numberOfGroups; ++i) {
+            Group group = new Group();
+            groups.add(group);
+        }
+        int turn = 0;
+        Group targetGroup;
+        while (availableVictims.size() > 0) {
+            targetGroup = groups.get(turn);
+            turn = (turn + 1) % groups.size();
+            Person victim = availableVictims.remove(0);
+            targetGroup.addMember(victim);
+        }
+        return groups;
+    }
 }
